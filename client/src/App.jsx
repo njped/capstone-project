ae39432bb7bdf425b3da1186ab0daee048bf3bf3
Isaac Messer <isaac.messer640.com>
2023-07-18 11:20:37 -0600

    Created additional Routes to additional pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home.jsx'
+import { NavBar } from './pages/components/NavBar.jsx'
+import { Courses } from './pages/Courses.jsx'
+import { UserInfo } from './pages/UserInfo.jsx'
+import { UserReg } from './pages/UserReg.jsx'
+import { Admin } from './pages/Admin.jsx'
 
 function App() {
 
   return (
     <>
+      <NavBar/>
       <Routes>
         <Route path='/' element={ <Home/> }/>
+        <Route path='/admin' element={ <Admin/> }/>
+        <Route path='courses' element={ <Courses/> }/>
+        <Route path='userinfo' element={ <UserInfo/> }/>
+        <Route path='userreg' element={ <UserReg/> }/>
       </Routes>
     </>
   )
