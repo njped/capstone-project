7e7fb365e2d85287563da0496c99508f44bcf8bf
Isaac Messer <isaac.messer640.com>
2023-07-19 10:50:11 -0600

    Added structure and LoginField.jsx component

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoginField } from "./components/LoginField";
+
+export function Login() {
+
+    return(
+        <>
+            <h1>Please Login:</h1>
+            <br />
+            <LoginField/>
+        </>
+    )
+}
