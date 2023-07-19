117e89e68e5f62db66beef5430dfa3dd45e69daf
Isaac Messer <isaac.messer640.com>
2023-07-19 10:48:38 -0600

    Changed Routes to default load Login.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import './App.css'
 
 import { Home } from './pages/Home.jsx'
-import { NavBar } from './pages/components/NavBar.jsx'
 import { Courses } from './pages/Courses.jsx'
 import { UserInfo } from './pages/UserInfo.jsx'
 import { UserReg } from './pages/UserReg.jsx'
 import { Admin } from './pages/Admin.jsx'
+import { Login } from './pages/Login.jsx'
 
 function App() {
 
   return (
     <>
-      <NavBar/>
       <Routes>
-        <Route path='/' element={ <Home/> }/>
-        <Route path='/admin' element={ <Admin/> }/>
+        <Route path='/' element={ <Login/> }/>
+        <Route path='home' element={ <Home/> }/>
+        <Route path='admin' element={ <Admin/> }/>
         <Route path='courses' element={ <Courses/> }/>
         <Route path='userinfo' element={ <UserInfo/> }/>
         <Route path='userreg' element={ <UserReg/> }/>
