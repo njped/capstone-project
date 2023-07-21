b6cf6360204320597d3b4a95a28c6ba56aeb2cbc
Isaac Messer <isaac.messer640.com>
2023-07-20 22:27:10 -0600

    Adjusted document layout

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import { LoginField } from "./components/LoginField";
+
+
 
 export function Login() {
 
@@ -8,6 +12,8 @@ export function Login() {
             <h1>Please Login:</h1>
             <br />
             <LoginField/>
+            <br />
+            <Link to='/user-reg'>Create Account</Link>
         </>
     )
 }
