0c1ac69c96fded7ead24dfa81071ac5b60413f25
Isaac Messer <isaac.messer640.com>
2023-07-21 12:23:29 -0600

    Updated logic to logiin created users and type

--- client/src/pages/components/LoginField.jsx
+++ client/src/pages/components/LoginField.jsx
@@ -8,41 +8,40 @@ export const LoginField = () => {
     
     const navigateTo = useNavigate();
 
-    let studentSavedUsername = 'RegularUser';
-    let studentSavedPassword = 'PasswordReg';
-    
-    let adminSavedUsername = 'AdminUser';
-    let adminSavedPassword = 'PasswordAdmin';
-
 
     function handleSubmit(Event) {
         Event.preventDefault();
 
-        console.log(username);
-        console.log(password);
+        let storageCondition = localStorage.getItem('Existing-Accounts') === null;
+        let userAccounts = storageCondition? [] : JSON.parse(localStorage.getItem('Existing-Accounts'));
+        let foundUser = userAccounts.filter(searchUsers);
+        
+        function searchUsers(item) {
+            return item.UserName === username && item.PassWord === password;
+        }
+        console.log(foundUser);
+        console.log(Object.keys(foundUser).length);
 
-        if( 
-            username === studentSavedUsername
-            && 
-            password === studentSavedPassword ) {
+        
+        if (Object.keys(foundUser).length === 1) {
+            if( foundUser[0].UserName === username && foundUser[0].PassWord === password) {
+                console.log(`User Found ${foundUser[0].FirstName} ${foundUser[0].LastName}: UUID - ${foundUser[0].UUID}`)
 
-            navigateTo('/home');
-
-        } else if( 
-            username === adminSavedUsername 
-            && 
-            password === adminSavedPassword ) {
-
-            navigateTo('/admin');
-
+                if( foundUser[0].isAdmin === true) {
+                    navigateTo('/admin')
+                } else if( foundUser[0].isAdmin === false) {
+                    navigateTo('/home')
+                } else(
+                    alert(`Cannot detemine isAdmin value of user`)
+                )
+            } 
+        } else if ( Object.keys(foundUser).length === 0){
+            alert(`--User not found--\n please try again or create an account if you havnt already.`)
+            console.log(`User Not Found`)
         } else {
-
-            alert('Username or Password are incorrect \nPlease try again');
-
+            alert(`Cannot detirmine if user has an account`)
         };
-
-
-    }
+    };
 
 
     return(
@@ -52,14 +51,17 @@ export const LoginField = () => {
                 <input
                     type="text" 
                     id="userNameInput"
+                    autoCapitalize="off"
+                    required={true}
                     onChange={ Event => setUsername( Event.target.value )}
 
                 />
                 <br />
                 <label htmlFor="passWordInput">Password: </label>
                 <input
-                    type="text"
+                    type="password"
                     id="passWordInput"
+                    required={true}
                     onChange={ Event => setPassword( Event.target.value )}
                 />
                 <br />
