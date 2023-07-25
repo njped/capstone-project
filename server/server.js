487346effd274389a4441b80f19f2e83a0aa520f
Nathan Pedigo <njpedigo@gmail.com>
2023-07-25 17:10:40 -0600

    created backend branch and merged it with dev branch

--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 // For production not development
-app.use(express.static("../client/src"));
+app.use("/", express.static(path.join(__dirname, "../client/")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
