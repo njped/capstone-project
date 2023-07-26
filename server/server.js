716ac7bf4c8ebccff0ff4015fbf3fa72e2759f3d
Nathan Pedigo <njpedigo@gmail.com>
2023-07-26 08:02:45 -0600

    commited out endpoints

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const passport = require('passport')
-const jwt = require('jwt')
+// const jwt = require('jwt')
 require('dotenv').config()
 const dbCourses = require('./db/courses.js');
 const dbProfile = require('./db/profile.js')
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 // For production not development
-app.use("/", express.static('../client/dist'));
+app.use("/", express.static('../client/src/pages'));
 
-app.get('/getActors', db.getActors);
-app.get('/getRental', db.getRental);
-app.post('/updateActors', db.updateActor);
-app.put('/updateActors', db.updateActor);
+// app.get('/getActors', db.getActors);
+// app.get('/getRental', db.getRental);
+// app.post('/updateActors', db.updateActor);
+// app.put('/updateActors', db.updateActor);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
