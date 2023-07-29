82e10bf5935f8fc7cf1cb84ead5710750de4d540
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-07-28 18:57:30 -0600

    Removed NavBar for testing

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavBar } from './components/NavBar.jsx'
+import { AdminManage } from "./components/AdminManage.jsx";
 
 export function Admin() {
 
     return(
         <>
-            <NavBar/>
+            {/* <NavBar/> */}
             <h1>Admin Page</h1>
+            <AdminManage/>
         </>
     )
 }
