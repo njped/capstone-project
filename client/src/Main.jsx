5e734bd7e7e202e0bff7558ea85a9d44169359b9
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-07-28 18:56:44 -0600

    Commented out StrictMode to stop doubles

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -6,9 +6,9 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
+  {/* </React.StrictMode>, */}
 )
