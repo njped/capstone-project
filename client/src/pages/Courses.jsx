b9706b823bf89267b26ed3acbb9f7727a747c1f5
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-08-13 15:36:13 -0600

    Added dynamic drop down model

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -12,7 +12,7 @@ export function Courses() {
                     <NavBar/>   
                 </div>
                 <div id="coursesPageManage">
-                    <CourseManage/>
+                   <CourseManage/>
                 </div>
             </div>
         </>
