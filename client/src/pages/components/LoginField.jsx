b990879cda6e8ba230a9f00bebb4dad0149be94a
Nathan Pedigo <njpedigo@gmail.com>
2023-08-14 07:59:04 -0600

    working on frontend just need to get the axios to work with posting

--- client/src/pages/components/LoginField.jsx
+++ client/src/pages/components/LoginField.jsx
@@ -2,88 +2,104 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 
 
 
-export default function LoginField () {
-    const [ username, setUsername ] = useState('')
-    const [ password, setPassword ] = useState('')
+export default function LoginField() {
     const navigateTo = useNavigate();
-    
+    const [inputValue, setInputValue] = useState({
+        email: "",
+        password: "",
+    });
+    const { username, password } = inputValue;
 
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        
-        const { userNameInput, passWordInput, } = this.state
-
-        fetch('http://localhost:5050/getUsers', {
-            method: "GET"
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data, "userRegister")
-            console.log(data[0].isAdmin, "admin")
-            for(let i = 0; i < data.length; i++){
-                console.log(data[i][isAdmin])
-            }
-        })
-        
-        fetch('http://localhost:5050/login', {
-            method: "POST",
-            crossDomain: true,
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify({
-                username: userNameInput,
-                password: passWordInput,
-            }),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data, "userRegister")
-            if(data.status === 'ok') {
-                window.localStorage.setItem("token", data.data)
-                window.localStorage.setItem("loggedIn", true)
-
-            }
-        })
-        // let storageCondition = localStorage.getItem('Existing-Accounts') === null;
-        // let userAccounts = storageCondition? [] : JSON.parse(localStorage.getItem('Existing-Accounts'));
-        // let foundUser = userAccounts.filter(searchUsers);
-        
-        // function searchUsers(item) {
-        //     return item.UserName === username && item.PassWord === password;
-        // }
-        // console.log(foundUser);
-        // console.log(Object.keys(foundUser).length);
-
-        
-        // if (Object.keys(foundUser).length === 1) {
-        //     if( foundUser[0].UserName === username && foundUser[0].PassWord === password) {
-        //         console.log(`User Found ${foundUser[0].FirstName} ${foundUser[0].LastName}: UUID - ${foundUser[0].UUID}`)
-
-        //         if( foundUser[0].isAdmin === true) {
-        //             navigateTo('/admin')
-        //         } else if( foundUser[0].isAdmin === false) {
-        //             navigateTo('/home')
-        //         } else(
-        //             alert(`Cannot detemine isAdmin value of user`)
-        //         )
-        //     } 
-        // } else if ( Object.keys(foundUser).length === 0){
-        //     alert(`--User not found--\n please try again or create an account if you havnt already.`)
-        //     console.log(`User Not Found`)
-        // } else {
-        //     alert(`Cannot detirmine if user has an account`)
-        // };
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value,
+        });
     };
 
-    return(
+    const handleError = (err) =>
+        toast.error(err, {
+            position: "bottom-left",
+        });
+    const handleSuccess = (msg) =>
+        toast.success(msg, {
+            position: "bottom-left",
+        });
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "http://localhost:5050/login",
+                {
+                    ...inputValue,
+                },
+                { withCredentials: true }
+            );
+            console.log(data);
+            const { success, message } = data;
+            if (success) {
+                handleSuccess(message);
+                setTimeout(() => {
+                    navigate("/");
+                }, 1000);
+            } else {
+                handleError(message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        setInputValue({
+            ...inputValue,
+            username: "",
+            password: "",
+        });
+
+
+    //     fetch('http://localhost:5050/getUsers', {
+    //         method: "GET"
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data, "userRegister")
+    //             console.log(data[0].isAdmin, "admin")
+    //             for (let i = 0; i < data.length; i++) {
+    //                 console.log(data[i][isAdmin])
+    //             }
+    //         })
+
+    //     fetch('http://localhost:5050/login', {
+    //         method: "POST",
+    //         crossDomain: true,
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json",
+    //             "Access-Control-Allow-Origin": "*",
+    //         },
+    //         body: JSON.stringify({
+    //             username: userNameInput,
+    //             password: passWordInput,
+    //         }),
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data, "userRegister")
+    //             if (data.status === 'ok') {
+    //                 window.localStorage.setItem("token", data.data)
+    //                 window.localStorage.setItem("loggedIn", true)
+
+    //             }
+    //         })
+    };
+
+    return (
         <>
             <div id="loginFormContainer">
                 <form id="loginForm" onSubmit={handleSubmit}>
@@ -91,20 +107,26 @@ export default function LoginField () {
                         <h1>Login</h1>
                     </div>
                     <div className="formInputs">
+                        <label htmlFor="username"></label>
                         <input
-                            type="text" 
+                            type="text"
                             id="userNameInput"
                             placeholder="Username"
                             required={true}
                             autoCapitalize="off"
-                            onChange={(e) => setUsername({ userNameInput: e.target.value })}
+                            name="username"
+                            value={username}
+                            onChange={handleOnChange}
                         />
+                        <label htmlFor="password"></label>
                         <input
                             type="password"
                             id="passWordInput"
                             placeholder="Password"
                             required={true}
-                            onChange={(e) => setPassword({ passWordInput: e.target.value })}
+                            name="email"
+                            value={email}
+                            onChange={handleOnChange}
                         />
                         <button type="submit">Go</button>
                     </div>
@@ -113,7 +135,6 @@ export default function LoginField () {
                     </div>
                 </form>
             </div>
-
         </>
     );
 };
--- client/src/pages/components/RegField.jsx
+++ client/src/pages/components/RegField.jsx
@@ -2,202 +2,253 @@ import React, { Component } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 
 
-export default function RegField () {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
-    const [address, setAddress] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [reEnterPassword, setReEnteredPassword] = useState('')
-    const [passwordType, setPasswordType] = useState('')
-    
+export default function RegField() {
+    const [passwordType, setPasswordType] = useState('password')
+    const navigate = useNavigate()
+    const [inputValue, setInputValue] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        address: "",
+        username: "",
+        password: "",
+        reEnterPassword: "",
+    })
+    const { firstName, lastName, email, phone, address, username, password, reEnterPassword } = inputValue
+
+    function handleOnChange(e) {
+        const { name, value } = e.target
+        setInputValue({ ...inputValue, [name]: value })
+    }
+
+    const handleError = (err) =>
+        toast.error(err, {
+            position: "bottom-left",
+        });
+    const handleSuccess = (msg) =>
+        toast.success(msg, {
+            position: "bottom-right",
+        });
 
     function handleClick(e) {
         e.preventDefault();
 
-        if(passwordType === 'password') {
+        if (passwordType === 'password') {
             setPasswordType('text');
         } else {
             setPasswordType('password');
         };
     };
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
-        // const { firstNameInput }  = setFirstName()
-        // const { lastNameInput } = setLastName()
-        // const { emailInput } = setEmail()
-        // const { phoneNumberInput } = setPhone()
-        // const { addressInput } = setAddress()
-        // const { userNameInput } = setUsername()
-        // const { passWordInput } = setPassword()
-        // const { passWordReEnterInput } = setReEnteredPassword()
 
         // Checking to see if there is a password
-        if(password === null || password === "")
-        {
+        if (password === null || password === "") {
             return alert("Password is required")
         }
-        if(reEnterPassword === null || reEnterPassword === "")
-        {
+        if (reEnterPassword === null || reEnterPassword === "") {
             return alert("Re-Enter Password is required")
         }
 
         // Checking Passwords are the same
-        if(passWordInput !== reEnterPassword)
-        {
+        if (password !== reEnterPassword) {
             return alert("Passwords are not the same")
         }
 
         // Checking email is typed
-        if(email === null || email === "")
-        {
+        if (email === null || email === "") {
             return alert("Email is required")
         }
 
         // Checking if username is typed
-        if(username === null || username === "")
-        {
+        if (username === null || username === "") {
             return alert("Username is required")
         }
 
-        fetch('http://localhost:5050/user-reg', {
-            method: "POST",
-            crossDomain: true,
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify({
-                firstName: firstNameInput, 
-                lastName: lastNameInput, 
-                email: emailInput, 
-                phone: phoneNumberInput,
-                address: addressInput,
-                username: userNameInput,
-                password: passWordInput,
-                reEnterPassword: passWordReEnterInput
-            }),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data, "userRegister")
-            // Checking to see if email is unique
-            if(data.status === "Email already in use") {
-                alert("Email already in use")
+        try {
+            const { data } = await axios.post(
+                "http://localhost:5050/user-reg",
+                {
+                    ...inputValue,
+                },
+                { withCredentials: true }
+            );
+            const { success, message } = data;
+            if (success) {
+                handleSuccess(message);
+                setTimeout(() => {
+                    navigate("/login");
+                }, 1000);
+            } else {
+                handleError(message);
             }
-            // Checking to see if username is unique
-            if(data.status === "Username already in use") {
-                alert("Username already in use")
-            }
-            if(data.status === 'ok') {
-                window.location.href = 'login'
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
+        } catch (error) {
+            console.log(error);
+        }
+        setInputValue({
+            ...inputValue,
+            email: "",
+            password: "",
+            username: "",
+        });
+
+        // fetch('http://localhost:5050/user-reg', {
+        //     method: "POST",
+        //     crossDomain: true,
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         Accept: "application/json",
+        //         "Access-Control-Allow-Origin": "*",
+        //     },
+        //     body: JSON.stringify({
+        //         firstName: firstNameInput,
+        //         lastName: lastNameInput,
+        //         email: emailInput,
+        //         phone: phoneNumberInput,
+        //         address: addressInput,
+        //         username: userNameInput,
+        //         password: passWordInput,
+        //         reEnterPassword: passWordReEnterInput
+        //     }),
+        // })
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         console.log(data, "userRegister")
+        //         // Checking to see if email is unique
+        //         if (data.status === "Email already in use") {
+        //             alert("Email already in use")
+        //         }
+        //         // Checking to see if username is unique
+        //         if (data.status === "Username already in use") {
+        //             alert("Username already in use")
+        //         }
+        //         if (data.status === 'ok') {
+        //             window.location.href = 'login'
+        //         }
+        //     })
+        //     .catch(error => {
+        //         console.log(error)
+        //     })
 
     };
-    
-        return (
+
+    return (
         <>
             <div id="regFormContainer">
                 <div id="regForm">
 
-                    <form onSubmit={this.handleSubmit}>
+                    <form onSubmit={handleSubmit}>
                         <div className="regFormHeader">
                             <h1>Registration</h1>
                         </div>
                         <div className="regFormInputs">
+                            <label htmlFor="firstName"></label>
                             <input
-                                type="text" 
+                                type="text"
                                 id="firstNameInput"
                                 placeholder="First Name"
-                                onChange={(e) => setFirstName({ firstNameInput: e.target.value })}
+                                value={firstName}
+                                name="firstName"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="lastName"></label>
                             <input
-                                type="text" 
+                                type="text"
                                 id="lastNameInput"
                                 placeholder="Last Name"
-                                onChange={(e) => setLastName({ lastNameInput: e.target.value })}
+                                value={lastName}
+                                name="lastName"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="email"></label>
                             <input
-                                type="email" 
+                                type="email"
                                 id="emailInput"
                                 placeholder="Email"
                                 required={true}
-                                onChange={(e) => setEmail({ emailInput: e.target.value })}
+                                value={email}
+                                name="email"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="phone"></label>
                             <input
-                                type="text" 
+                                type="text"
                                 id="phoneNumberInput"
                                 pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                 placeholder="Ex: 801-000-000"
-                                onChange={(e) => setPhone({ phoneNumberInput: e.target.value })}
+                                value={phone}
+                                name="phone"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="address"></label>
                             <input
-                                type="text" 
+                                type="text"
                                 id="addressInput"
                                 placeholder="Address"
-                                onChange={(e) => setAddress({ addressInput: e.target.value })}
+                                value={address}
+                                name="address"
+                                onChange={handleOnChange}
                             />
 
+                            <label htmlFor="username"></label>
                             <input
-                                type="text" 
+                                type="text"
                                 id="userNameInput"
                                 placeholder="Username"
                                 required={true}
-                                onChange={(e) => setUsername({ userNameInput: e.target.value })}
+                                value={username}
+                                name="username"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="password"></label>
                             <input
-                                type={this.state.passwordType} 
+                                type={passwordType}
                                 id="passWordInput"
                                 placeholder="Password"
                                 required={true}
-                                onChange={(e) => setPassword({ passWordInput: e.target.value })}
+                                value={password}
+                                name="password"
+                                onChange={handleOnChange}
                             />
 
-
+                            <label htmlFor="reEnterPassword"></label>
                             <input
-                                type={this.state.passwordType} 
+                                type={passwordType}
                                 id="passWordReEnterInput"
                                 placeholder="Reenter Password"
                                 required={true}
-                                onChange={(e) => setReEnteredPassword({ passWordReEnterInput: e.target.value })}
+                                value={reEnterPassword}
+                                name="reEnterPassword"
+                                onChange={handleOnChange}
                             />
                             <div className="regFormButtons">
-                                <button type="submit" onClick={handleSubmit}>Create User</button>
+                                <button type="submit">Create User</button>
                                 <button className='togglePasswordVisability' onClick={handleClick}>
-                                    { passwordType==="password"? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} /> }
+                                    {passwordType === "password" ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
                                 </button>
                             </div>
                         </div>
                         <div className="regFormLink">
                             <Link to='/'>User Login</Link>
-                        </div>           
+                        </div>
                     </form>
+                    <ToastContainer />
                 </div>
-            </div>  
+            </div>
         </>
     )
 }
